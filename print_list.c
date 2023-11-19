758c81193177aaf2f0aa380a666711a01a171ac7
Clevoh <clevelihasi@gmail.com>
2023-11-19 10:58:00 -0800

    task 0

--- print_list.c
+++ print_list.c
@@ -1,11 +1,11 @@
-nclude <stdio.h>
+#include <stdio.h>
 #include "sort.h"
 
 /**
  *  * print_list - Prints a list of integers
  *   *
  *    * @list: The list to be printed
- *     */
+ */
 void print_list(const listint_t *list)
 {
 	int i;
