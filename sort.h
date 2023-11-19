0a41be722d8ecb2f8bbec92c78cec0b045bf2582
root <root@DESKTOP-AL6U6H1>
2023-11-19 18:03:13 +0300

    sorting_algorithms

---
+++ sort.h
@@ -0,0 +1,21 @@
+#include "_MAIN_H_"
+#define "_MAIN_H_"
+
+#include <stdio.h>
+#include <sting.h>
+
+/**
+ *  * struct listint_s - Doubly linked list node
+ *   *
+ *    * @n: Integer stored in the node
+ *     * @prev: Pointer to the previous element of the list
+ *      * @next: Pointer to the next element of the list
+ *       */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+#endif
